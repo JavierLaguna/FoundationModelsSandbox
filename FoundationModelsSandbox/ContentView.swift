807cd6fcffc25a612dda1e9a3df508c655d762ef
Javier Laguna <javier.laguna@innocv.com>
2026-04-27 15:25:10 +0200

    Initial Commit

---
+++ FoundationModelsSandbox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoundationModelsSandbox
+//
+//  Created by Javier Laguna on 27/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoundationModelsSandbox/FoundationModelsSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationModelsSandboxApp.swift
+//  FoundationModelsSandbox
+//
+//  Created by Javier Laguna on 27/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct FoundationModelsSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
